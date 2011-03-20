62f102b1b6d3dc5cc21ca942a49b7b9aa12716ee
dleshem <dleshem@0c45ae61-e402-b38f-497d-22b0f2cd638a>
2011-03-20 10:12:48 +0000

    Extended ColorScheme (now supports customization of menu categories)

--- openrest4j-api/src/com/googlecode/openrest/ColorScheme.java
+++ openrest4j-api/src/com/googlecode/openrest/ColorScheme.java
@@ -10,18 +10,25 @@ public class ColorScheme implements Serializable {
     public static final String THEME_LIGHT = "light";
     public static final String THEME_DARK = "dark";
 
-    public ColorScheme(String theme, String background, String font, String border, String imageBackground,
-            String buttonFont, String buttonUp, String buttonDown, String buttonOver) {
+    public ColorScheme(String theme, String background, String font, String border,
+            String imageBackground, String imageBorder,
+            String buttonFont, String buttonUp, String buttonDown, String buttonOver,
+            String categoryFont, String categoryUp, String categoryDown, String categoryOver) {
 
         this.theme = theme;
         this.background = background;
         this.font = font;
         this.border = border;
         this.imageBackground = imageBackground;
+        this.imageBorder = imageBorder;
         this.buttonFont = buttonFont;
         this.buttonUp = buttonUp;
         this.buttonDown = buttonDown;
         this.buttonOver = buttonOver;
+        this.categoryFont = categoryFont;
+        this.categoryUp = categoryUp;
+        this.categoryDown = categoryDown;
+        this.categoryOver = categoryOver;
     }
 
     /** Default constructor for JSON deserialization. */
@@ -42,6 +49,9 @@ public class ColorScheme implements Serializable {
     @JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
     public String imageBackground;
 
+    @JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
+    public String imageBorder;
+
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public String buttonFont;
 
@@ -53,6 +63,18 @@ public class ColorScheme implements Serializable {
 
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public String buttonOver;
+
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+    public String categoryFont;
+
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+    public String categoryUp;
+
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+    public String categoryDown;
+    
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+    public String categoryOver;
 
     @Override
     public boolean equals(Object obj) {
@@ -78,6 +100,9 @@ public class ColorScheme implements Serializable {
         if ((this.imageBackground == null) ? (other.imageBackground != null) : !this.imageBackground.equals(other.imageBackground)) {
             return false;
         }
+        if ((this.imageBorder == null) ? (other.imageBorder != null) : !this.imageBorder.equals(other.imageBorder)) {
+            return false;
+        }
         if ((this.buttonFont == null) ? (other.buttonFont != null) : !this.buttonFont.equals(other.buttonFont)) {
             return false;
         }
@@ -90,23 +115,40 @@ public class ColorScheme implements Serializable {
         if ((this.buttonOver == null) ? (other.buttonOver != null) : !this.buttonOver.equals(other.buttonOver)) {
             return false;
         }
+        if ((this.categoryFont == null) ? (other.categoryFont != null) : !this.categoryFont.equals(other.categoryFont)) {
+            return false;
+        }
+        if ((this.categoryUp == null) ? (other.categoryUp != null) : !this.categoryUp.equals(other.categoryUp)) {
+            return false;
+        }
+        if ((this.categoryDown == null) ? (other.categoryDown != null) : !this.categoryDown.equals(other.categoryDown)) {
+            return false;
+        }
+        if ((this.categoryOver == null) ? (other.categoryOver != null) : !this.categoryOver.equals(other.categoryOver)) {
+            return false;
+        }
         return true;
     }
 
     @Override
     public int hashCode() {
         int hash = 5;
-        hash = 37 * hash + (this.theme != null ? this.theme.hashCode() : 0);
-        hash = 37 * hash + (this.background != null ? this.background.hashCode() : 0);
-        hash = 37 * hash + (this.font != null ? this.font.hashCode() : 0);
-        hash = 37 * hash + (this.border != null ? this.border.hashCode() : 0);
-        hash = 37 * hash + (this.imageBackground != null ? this.imageBackground.hashCode() : 0);
-        hash = 37 * hash + (this.buttonFont != null ? this.buttonFont.hashCode() : 0);
-        hash = 37 * hash + (this.buttonUp != null ? this.buttonUp.hashCode() : 0);
-        hash = 37 * hash + (this.buttonDown != null ? this.buttonDown.hashCode() : 0);
-        hash = 37 * hash + (this.buttonOver != null ? this.buttonOver.hashCode() : 0);
+        hash = 59 * hash + (this.theme != null ? this.theme.hashCode() : 0);
+        hash = 59 * hash + (this.background != null ? this.background.hashCode() : 0);
+        hash = 59 * hash + (this.font != null ? this.font.hashCode() : 0);
+        hash = 59 * hash + (this.border != null ? this.border.hashCode() : 0);
+        hash = 59 * hash + (this.imageBackground != null ? this.imageBackground.hashCode() : 0);
+        hash = 59 * hash + (this.imageBorder != null ? this.imageBorder.hashCode() : 0);
+        hash = 59 * hash + (this.buttonFont != null ? this.buttonFont.hashCode() : 0);
+        hash = 59 * hash + (this.buttonUp != null ? this.buttonUp.hashCode() : 0);
+        hash = 59 * hash + (this.buttonDown != null ? this.buttonDown.hashCode() : 0);
+        hash = 59 * hash + (this.buttonOver != null ? this.buttonOver.hashCode() : 0);
+        hash = 59 * hash + (this.categoryFont != null ? this.categoryFont.hashCode() : 0);
+        hash = 59 * hash + (this.categoryUp != null ? this.categoryUp.hashCode() : 0);
+        hash = 59 * hash + (this.categoryDown != null ? this.categoryDown.hashCode() : 0);
+        hash = 59 * hash + (this.categoryOver != null ? this.categoryOver.hashCode() : 0);
         return hash;
     }
-    
+
     private static final long serialVersionUID = 1L;
 }
