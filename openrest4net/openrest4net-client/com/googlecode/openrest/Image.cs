2ce5830c880c6274dce3355a1398bccebce60778
dleshem <dleshem@gmail.com>
2011-03-28 16:34:09 +0000

    Initial implementation of openrest4net-client.

---
+++ openrest4net/openrest4net-client/com/googlecode/openrest/Image.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace com.googlecode.openrest
+{
+    /**
+     * A raw image.
+     * @author DL
+     */
+    class Image
+    {
+        public Image(string contentType, byte[] content)
+        {
+            this.contentType = contentType;
+            this.content = content;
+        }
+
+        public string ContentType
+        {
+            get
+            {
+                return contentType;
+            }
+        }
+
+        public byte[] Content
+        {
+            get
+            {
+                return content;
+            }
+        }
+
+        private readonly string contentType;
+        private readonly byte[] content;
+    }
+}
---
+++ openrest4net/openrest4net-client/com/googlecode/openrest/RestJsonHttpException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace com.googlecode.openrest
+{
+    /**
+     * An exception thrown by a RESTful HTTP server, with an optional returned value.
+     * @author DL
+     */
+    class RestJsonHttpException : IOException
+    {
+        public RestJsonHttpException(int httpErrorCode, Object value)
+        {
+            this.httpErrorCode = httpErrorCode;
+            this.value = value;
+        }
+
+        public int HttpErrorCode {
+            get
+            {
+                return httpErrorCode;
+            }
+        }
+    
+        public Object Value {
+            get
+            {
+                return value;
+            }
+        }
+
+        private readonly int httpErrorCode;
+        private readonly Object value;
+    }
+}
