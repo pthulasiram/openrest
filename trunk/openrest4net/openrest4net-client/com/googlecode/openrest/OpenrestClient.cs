0c2200934e780c217bfdd6aa9fd223aa97cc681a
dleshem <dleshem@0c45ae61-e402-b38f-497d-22b0f2cd638a>
2011-03-30 22:14:55 +0000

    Implemented openrest4net (no support for images yet).

--- trunk/openrest4net/openrest4net-client/com/googlecode/openrest/OpenrestClient.cs
+++ trunk/openrest4net/openrest4net-client/com/googlecode/openrest/OpenrestClient.cs
@@ -71,6 +71,173 @@ namespace com.googlecode.openrest
 
         ///////////////////////////////////////////////////////////////////////
 
+        public Menu Menu
+        {
+            get
+            {
+                return Get<Menu>(new Uri(restaurantUri + "/menu"));
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        public Staff Staff
+        {
+            get
+            {
+                return Get<Staff>(new Uri(restaurantUri + "/staff/" + "?access_token=" + accessToken));
+            }
+        }
+
+        public Staff SetStaff(Staff staff)
+        {
+            return Set(new Uri(restaurantUri + "/staff/" + "?access_token=" + accessToken), staff);
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        public Notifications Notifications
+        {
+            get
+            {
+                return Get<Notifications>(new Uri(restaurantUri + "/notifications/" + "?access_token=" + accessToken));
+            }
+        }
+
+        public Notifications SetNotifications(Notifications notifications)
+        {
+            return Set(new Uri(restaurantUri + "/notifications/" + "?access_token=" + accessToken), notifications);
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        public Menu Items
+        {
+            get
+            {
+                return Get<Menu>(new Uri(restaurantUri + "/items/"));
+            }
+        }
+
+        public Item Item(string itemId)
+        {
+            return Get<Item>(new Uri(restaurantUri + "/items/" + itemId));
+        }
+
+        public Item SetItem(string itemId, Item item)
+        {
+            return Set<Item>(new Uri(restaurantUri + "/items/" + itemId + "?access_token=" + accessToken), item);
+        }
+
+        public Item AddItem(Item item)
+        {
+            return Add<Item>(new Uri(restaurantUri + "/items/" + "?access_token=" + accessToken), item);
+        }
+
+        public Image ItemImage(string itemId)
+        {
+            return RestJsonClient.GetImage<Response<object>>(new Uri(restaurantUri + "/items/" + itemId + "/picture"));
+        }
+
+        public void SetItemImage(string itemId, string imageFilename, Image image)
+        {
+            RestJsonClient.Put<Response<object>>(
+                new Uri(restaurantUri + "/items/" + itemId + "/picture" + "?access_token=" + accessToken),
+                imageFilename, image);
+        }
+
+        public void RemoveItemImage(string itemId)
+        {
+            Remove(new Uri(restaurantUri + "/items/" + itemId + "/picture" + "?access_token=" + accessToken));
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        public Menu Tags
+        {
+            get
+            {
+                return Get<Menu>(new Uri(restaurantUri + "/tags/"));
+            }
+        }
+
+        public Tag Tag(string tagId)
+        {
+            return Get<Tag>(new Uri(restaurantUri + "/tags/" + tagId));
+        }
+
+        public Tag SetTag(string tagId, Tag tag)
+        {
+            return Set<Tag>(new Uri(restaurantUri + "/tags/" + tagId + "?access_token=" + accessToken), tag);
+        }
+
+        public Tag AddTag(Tag tag)
+        {
+            return Add<Tag>(new Uri(restaurantUri + "/tags/" + "?access_token=" + accessToken), tag);
+        }
+
+        public void RemoveTag(string tagId)
+        {
+            Remove(new Uri(restaurantUri + "/tags/" + tagId + "?access_token=" + accessToken));
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        public Menu Categories
+        {
+            get
+            {
+                return Get<Menu>(new Uri(restaurantUri + "/categories/"));
+            }
+        }
+
+        public Menu SetCategories(Menu categoriesMenu)
+        {
+            return Set<Menu>(new Uri(restaurantUri + "/categories/" + "?access_token=" + accessToken), categoriesMenu);
+        }
+
+        public Category Category(string categoryId)
+        {
+            return Get<Category>(new Uri(restaurantUri + "/categories/" + categoryId));
+        }
+
+        public Category SetCategory(string categoryId, Category category)
+        {
+            return Set<Category>(new Uri(restaurantUri + "/categories/" + categoryId + "?access_token=" + accessToken), category);
+        }
+
+        public Category AddCategory(Category category)
+        {
+            return Add<Category>(new Uri(restaurantUri + "/categories/" + "?access_token=" + accessToken), category);
+        }
+
+        public void RemoveCategory(string categoryId)
+        {
+            Remove(new Uri(restaurantUri + "/categories/" + categoryId + "?access_token=" + accessToken));
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        public IList<Order> Orders
+        {
+            get
+            {
+                return Get<IList<Order>>(new Uri(restaurantUri + "/orders/" + "?access_token=" + accessToken));
+            }
+        }
+
+        public Order Order(string orderId)
+        {
+            return Get<Order>(new Uri(restaurantUri + "/orders/" + orderId + "?access_token=" + accessToken));
+        }
+
+        public OrderConfirmation AddOrder(Order order)
+        {
+            return Add<Order, OrderConfirmation>(new Uri(restaurantUri + "/orders/"), order);
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
         private static T Get<T>(Uri uri)
         {
             try
@@ -111,12 +278,17 @@ namespace com.googlecode.openrest
 
         private static T Add<T>(Uri uri, T value)
         {
+            return Add<T, T>(uri, value);
+        }
+
+        private static R Add<T, R>(Uri uri, T value)
+        {
             try
             {
-                return RestJsonClient.Post<Response<T>>(uri, value).value;
+                return RestJsonClient.Post<Response<R>>(uri, value).value;
             }
-            catch (RestJsonHttpException<Response<T>> e) {
-                Response<T> response = e.Value;
+            catch (RestJsonHttpException<Response<R>> e) {
+                Response<R> response = e.Value;
                 if (response != null)
                 {
                     throw new OpenrestException(e.HttpStatusCode, response.error, response.errorMessage, e);
