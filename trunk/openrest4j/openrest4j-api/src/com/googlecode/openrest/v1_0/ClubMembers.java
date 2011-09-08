bb1915b37cf23507e584b93927a3d6aecfa59deb
dleshem <dleshem@0c45ae61-e402-b38f-497d-22b0f2cd638a>
2011-09-08 08:19:23 +0000

    Changed package to v1.0

---
+++ trunk/openrest4j/openrest4j-api/src/com/googlecode/openrest/v1_0/ClubMembers.java
@@ -0,0 +1,23 @@
+package com.googlecode.openrest.v1_0;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ClubMembers implements Serializable {
+    public ClubMembers(List<ClubMember> clubMembers) {
+        this.clubMembers = clubMembers;
+    }
+
+    /** Default constructor for JSON deserialization. */
+    public ClubMembers() {}
+
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_DEFAULT)
+    public List<ClubMember> clubMembers = Collections.emptyList();
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ trunk/openrest4j/openrest4j-api/src/com/googlecode/openrest/v1_0/Staff.java
@@ -0,0 +1,63 @@
+package com.googlecode.openrest.v1_0;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Staff implements Serializable {
+    public static final String STAFF_ADMIN = "admin";
+    public static final String STAFF_MANAGER = "manager";
+    public static final String STAFF_EMPLOYEE = "employee";
+    
+    /** All known staff types. */
+    public static final Set<String> ALL_STAFFS = new HashSet<String>(Arrays.asList(new String[] {
+    		STAFF_ADMIN, STAFF_MANAGER, STAFF_EMPLOYEE
+    }));
+
+    public Staff(Map<String, List<String>> staff) {
+        this.staff = staff;
+    }
+
+    /** Default constructor for JSON deserialization. */
+    public Staff() {}
+
+    public static Staff createAdmins(List<String> adminStaff) {
+        final Map<String, List<String>> staff = new HashMap<String, List<String>>(1);
+        staff.put(STAFF_ADMIN, adminStaff);
+        return new Staff(staff);
+    }
+
+    public static Staff createManagers(List<String> managerStaff) {
+        final Map<String, List<String>> staff = new HashMap<String, List<String>>(1);
+        staff.put(STAFF_MANAGER, managerStaff);
+        return new Staff(staff);
+    }
+
+    public static Staff createEmployees(List<String> employeeStaff) {
+        final Map<String, List<String>> staff = new HashMap<String, List<String>>(1);
+        staff.put(STAFF_EMPLOYEE, employeeStaff);
+        return new Staff(staff);
+    }
+
+    public static Staff createRestaurantStaff(List<String> managerStaff, List<String> employeeStaff) {
+        final Map<String, List<String>> staff = new HashMap<String, List<String>>(2);
+        staff.put(STAFF_MANAGER, managerStaff);
+        staff.put(STAFF_EMPLOYEE, employeeStaff);
+        return new Staff(staff);
+    }
+
+    /** Maps roles (see above) to list of user-ids. */
+    @JsonSerialize(include=JsonSerialize.Inclusion.NON_DEFAULT)
+    public Map<String, List<String>> staff = Collections.emptyMap();
+
+    private static final long serialVersionUID = 1L;
+}
