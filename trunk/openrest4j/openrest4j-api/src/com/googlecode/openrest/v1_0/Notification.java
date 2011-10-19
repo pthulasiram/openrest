bc1a687b7c8e52548d2d309e6689dbdaf9b30048
dleshem@gmail.com <dleshem@gmail.com@0c45ae61-e402-b38f-497d-22b0f2cd638a>
2011-10-19 14:27:53 +0000

    Added Notification.acceptOrder

--- trunk/openrest4j/openrest4j-api/src/com/googlecode/openrest/v1_0/Notification.java
+++ trunk/openrest4j/openrest4j-api/src/com/googlecode/openrest/v1_0/Notification.java
@@ -10,7 +10,7 @@ import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Notification implements Serializable {
-    /** Triggered when a new order is received. Duration is always 0. */
+	/** Triggered when a new order is received. Duration is always 0. */
     public static final String NOTIFICATION_TYPE_NEW_ORDER = "new_order";
     /** Triggered when a new order is not handled for some duration. */
     public static final String NOTIFICATION_TYPE_DELAYED_ORDER = "delayed_order";
@@ -23,23 +23,25 @@ public class Notification implements Serializable {
     }));
 
     /** Constructs a previously submitted notification from persisted data. */
-    public Notification(String restaurantId, String type, Contact contact, Integer durationMins) {
+    public Notification(String restaurantId, String type, Contact contact,
+    		Integer durationMins, Boolean acceptOrder) {
         this.restaurantId = restaurantId;
         this.type = type;
         this.contact = contact;
         this.durationMins = durationMins;
+        this.acceptOrder = acceptOrder;
     }
 
     /** Constructs a new notification to be submitted. */
-    public Notification(String type, Contact contact, Integer durationMins) {
-        this(null, type, contact, durationMins);
+    public Notification(String type, Contact contact, Integer durationMins, Boolean acceptOrder) {
+        this(null, type, contact, durationMins, acceptOrder);
     }
     
     /** Default constructor for JSON deserialization. */
     public Notification() {}
 
-    public Notification delayedOrderNotification(Contact contact, Integer durationMins) {
-        return new Notification(NOTIFICATION_TYPE_DELAYED_ORDER, contact, durationMins);
+    public Notification delayedOrderNotification(Contact contact, Integer durationMins, Boolean acceptOrder) {
+        return new Notification(NOTIFICATION_TYPE_DELAYED_ORDER, contact, durationMins, acceptOrder);
     }
 
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
@@ -59,6 +61,20 @@ public class Notification implements Serializable {
      */
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_DEFAULT)
     public Integer durationMins = 0;
+    
+    /**
+     * For orders-related notifications, whether or not the order should be
+     * marked as accepted upon successful transmission of the notification.
+     */
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_DEFAULT)
+    public Boolean acceptOrder = Boolean.FALSE;
+    
+    @Override
+	public String toString() {
+		return "Notification [restaurantId=" + restaurantId + ", type=" + type
+				+ ", contact=" + contact + ", durationMins=" + durationMins
+				+ ", acceptOrder=" + acceptOrder + "]";
+	}
 
     private static final long serialVersionUID = 1L;
 }
