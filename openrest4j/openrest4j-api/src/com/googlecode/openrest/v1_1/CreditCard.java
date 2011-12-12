eee4bb47905b0bc5e76a6c2822bc129bb4264572
dleshem <dleshem@0c45ae61-e402-b38f-497d-22b0f2cd638a>
2011-12-12 08:56:39 +0000

    Added SavedPayment.

--- openrest4j/openrest4j-api/src/com/googlecode/openrest/v1_1/CreditCard.java
+++ openrest4j/openrest4j-api/src/com/googlecode/openrest/v1_1/CreditCard.java
@@ -7,6 +7,8 @@ import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreditCard implements Serializable, Cloneable {
+    private static final long serialVersionUID = 1L;
+    
 	public CreditCard(String number, Integer expireMonth, Integer expireYear,
             String holderId, String holderName, Boolean anonymized) {
         this.number = number;
@@ -25,24 +27,26 @@ public class CreditCard implements Serializable, Cloneable {
     	return new CreditCard(number, expireMonth, expireYear, holderId, holderName, anonymized);
 	}
 
+    /** The card number (digits only). */
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public String number;
 
-    /** 1-based */
+    /** Card expiration month (1-based). Non-expiring cards can ignore this. */
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public Integer expireMonth;
 
+    /** Card expiration year. Non-expiring cards can ignore this. */
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public Integer expireYear;
 
+    /** Optional card holder-id (e.g. government issued unique identity card number). */
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public String holderId;
 
+    /** Card holder name (required). */
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public String holderName;
 
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_DEFAULT)
     public Boolean anonymized = Boolean.FALSE;
-
-    private static final long serialVersionUID = 1L;
 }
