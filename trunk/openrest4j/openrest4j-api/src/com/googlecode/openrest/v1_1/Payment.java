f4c57cfd6164d8d2f44bd642c380b831fa37cb32
dleshem <dleshem@0c45ae61-e402-b38f-497d-22b0f2cd638a>
2011-12-12 09:03:34 +0000

    Merged SavedPayment into Payment.

--- trunk/openrest4j/openrest4j-api/src/com/googlecode/openrest/v1_1/Payment.java
+++ trunk/openrest4j/openrest4j-api/src/com/googlecode/openrest/v1_1/Payment.java
@@ -24,10 +24,21 @@ public class Payment implements Serializable {
     		PAYMENT_TYPE_CASH, PAYMENT_TYPE_CREDIT, PAYMENT_TYPE_DEBIT, PAYMENT_TYPE_10BIS
     }));
 
+    /** Constructs a new one-time payment. */
     public Payment(String type, Integer amount, CreditCard card) {
         this.type = type;
         this.amount = amount;
         this.card = card;
+    }
+    
+    /** Constructs a user's saved payment. */
+    public Payment(String id, String userId, String password, String type, CreditCard card) {
+    	this.id = id;
+    	this.userId = userId;
+    	this.password = password;
+    	this.type = type;
+    	this.card = card;
+    	// amount defaults to 0
     }
 
     /** Default constructor for JSON deserialization. */
@@ -44,6 +55,23 @@ public class Payment implements Serializable {
     /** Credit card details (not valid for PAYMENT_TYPE_CASH or PAYMENT_CARD_DEBIT) */
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public CreditCard card;
+    
+    /** The user's Facebook id (for saved payments). */
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+    public String userId;
+    
+    /** Payment unique id (for saved payments). */
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+    public String id;
+
+    /**
+     * Optional user-defined password for protecting against unauthorized usage (for saved payments).
+     * 
+     * For anonymized saved payments, this would either be missing (null) to indicate
+     * no password, or empty ("") to indicate a password exists and was anonymized.
+     */
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+    public String password;
 
     private static final long serialVersionUID = 1L;
 }
