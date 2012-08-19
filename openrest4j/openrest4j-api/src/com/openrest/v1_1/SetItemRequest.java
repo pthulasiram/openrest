03e63b8866b57d4f6f7ac62b236178f9fbcb6b36
dleshem <dleshem@gmail.com>
2012-08-19 16:27:17 +0000

    - Added various credit card forms. - Added Notification.NOTIFICATION_TYPE_FEEDBACK - Added DeferredRequest and SetItemRequest

---
+++ openrest4j/openrest4j-api/src/com/openrest/v1_1/SetItemRequest.java
@@ -0,0 +1,25 @@
+package com.openrest.v1_1;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import com.googlecode.openrest.v1_1.Item;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SetItemRequest extends Request {
+    private static final long serialVersionUID = 1L;
+    
+    /** Default constructor for JSON deserialization. */
+    public SetItemRequest() {}
+    
+    public SetItemRequest(String accessToken, Item item) {
+    	this.accessToken = accessToken;
+    	this.item = item;
+    }
+    
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+    public String accessToken;
+    
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+    public Item item;
+}
