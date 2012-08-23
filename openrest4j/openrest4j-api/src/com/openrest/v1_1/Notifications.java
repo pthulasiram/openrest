a237447644748b38fc9d04f79ba9bf76d84c5162
dleshem <dleshem@gmail.com>
2012-08-23 13:04:04 +0000

    - Added GetNotificationsRequest, SetNotificationsRequest - Added Role.organizationType

---
+++ openrest4j/openrest4j-api/src/com/openrest/v1_1/Notifications.java
@@ -0,0 +1,25 @@
+package com.openrest.v1_1;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import com.googlecode.openrest.v1_1.Notification;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Notifications implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    public Notifications(List<Notification> notifications) {
+        this.notifications = notifications;
+    }
+
+    /** Default constructor for JSON deserialization. */
+    public Notifications() {}
+
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_DEFAULT)
+    public List<Notification> notifications = Collections.emptyList();
+}
