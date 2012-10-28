900da9ebb8ec3578a5ece98e91cdea7bd2a7baa7
dleshem@gmail.com <dleshem@gmail.com@0c45ae61-e402-b38f-497d-22b0f2cd638a>
2012-10-28 10:34:01 +0000

    - Date is now Comparable. - Added AddItemRequest, SetTagRequest, SetCategoriesRequest

--- trunk/openrest4j/openrest4j-api/src/com/openrest/v1_1/Date.java
+++ trunk/openrest4j/openrest4j-api/src/com/openrest/v1_1/Date.java
@@ -8,7 +8,9 @@ import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Date implements Serializable, Cloneable {
+public class Date implements Serializable, Cloneable, Comparable<Date> {
+	private static final long serialVersionUID = 1L;
+    
 	public Date(Integer year, Integer month, Integer day, Integer hour, Integer minute) {
         this.year = year;
         this.month = month;
@@ -44,6 +46,80 @@ public class Date implements Serializable, Cloneable {
     	return String.format("%04d-%02d-%02d %02d:%02d",
     			year, month, day, hour, minute);
 	}
+    
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((day == null) ? 0 : day.hashCode());
+		result = prime * result + ((hour == null) ? 0 : hour.hashCode());
+		result = prime * result + ((minute == null) ? 0 : minute.hashCode());
+		result = prime * result + ((month == null) ? 0 : month.hashCode());
+		result = prime * result + ((year == null) ? 0 : year.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		if (day == null) {
+			if (other.day != null)
+				return false;
+		} else if (!day.equals(other.day))
+			return false;
+		if (hour == null) {
+			if (other.hour != null)
+				return false;
+		} else if (!hour.equals(other.hour))
+			return false;
+		if (minute == null) {
+			if (other.minute != null)
+				return false;
+		} else if (!minute.equals(other.minute))
+			return false;
+		if (month == null) {
+			if (other.month != null)
+				return false;
+		} else if (!month.equals(other.month))
+			return false;
+		if (year == null) {
+			if (other.year != null)
+				return false;
+		} else if (!year.equals(other.year))
+			return false;
+		return true;
+	}
+	
+	private static int compare(Integer int1, Integer int2) {
+		if (int1 != null) {
+			return ((int2 != null) ? int1.compareTo(int2) : 1);
+		} else {
+			return ((int2 != null) ? -1 : 0);
+		}
+	}
+	
+	public int compareTo(Date other) {
+		int result = compare(year, other.year);
+		if (result == 0) {
+			result = compare(month, other.month);
+			if (result == 0) {
+				result = compare(day, other.day);
+				if (result == 0) {
+					result = compare(hour, other.hour);
+					if (result == 0) {
+						result = compare(minute, other.minute);
+					}
+				}
+			}
+		}
+		return result;
+	}
 
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public Integer year;
@@ -60,6 +136,4 @@ public class Date implements Serializable, Cloneable {
 
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public Integer minute;
-    
-    private static final long serialVersionUID = 1L;
 }
