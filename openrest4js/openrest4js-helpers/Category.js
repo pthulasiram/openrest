2a57415a51ce220f94e04894fe5a7f4a10844e5c
amit.yoav <amit.yoav@gmail.com>
2013-04-23 14:20:14 +0000

    The different getStatus functions now return the real 'until' even if followed by an availability with the same status

--- openrest4js/openrest4js-helpers/Category.js
+++ openrest4js/openrest4js-helpers/Category.js
@@ -3,10 +3,52 @@ var openrest = openrest || {};
 openrest.CategoryHelper = openrest.CategoryHelper || (function() {
 	var self = {};
 
+    function getNextTimeStatusIs(timezone, times, wantedStatus, _time)
+    {
+        var time = new timezoneJS.Date();
+        time.setTimezone(timezone);
+        time.setTimestampToNow();
+
+        if (_time)
+        {
+            time.setTimezone(_time.getTimezone());
+            time.setTime(_time.getTime());
+        }
+
+        var when = time.getTime() || Number.MAX_VALUE;
+
+        var times = times || {weekly:[], exceptions:[]};
+        var util = new availability.AvailabilityIterator({
+        	cal : time,
+        	availability : times
+        });
+
+        if (!util.hasNext())
+        {
+            return {when:Number.MAX_VALUE};
+        }
+
+        var status = util.next();
+        var reason = status.reason;
+        var comment = status.comment;
+        while (status.status !== wantedStatus)
+        {
+            if (!status.until) return {when:Number.MAX_VALUE};
+
+            when = status.until;
+            reason = status.reason;
+            comment = status.comment;
+
+        	status = util.next();
+        }
+        return {when:when, reason:reason, comment:comment}
+    }
+
     self.getStatus = function(params)
     {
         var category = params.category;
-        var timezone = params.timezone;
+        var time = params.time;
+        var timezone = params.timezone || time.getTimezone();
 
         if (!category.properties)
         {
@@ -15,30 +57,18 @@ openrest.CategoryHelper = openrest.CategoryHelper || (function() {
 
         var properties = JSON.parse(category.properties["com.implied2.spice"] || "{\"inactive\":false}");
         var times = properties.availability || {weekly:[], exceptions:[]};
-   
-        if (properties.inactive)
+
+        var nextAvailable = getNextTimeStatusIs(timezone, times, OPENREST_STATUS_STATUS_AVAILABLE, time);
+        var nextUnavailable = getNextTimeStatusIs(timezone, times, OPENREST_STATUS_STATUS_UNAVAILABLE, time);
+
+        if (nextAvailable.when < nextUnavailable.when)
         {
-            return {'status': OPENREST_STATUS_STATUS_UNAVAILABLE, until:Number.MAX_VALUE}; 
+            return {status:OPENREST_STATUS_STATUS_AVAILABLE, until:nextUnavailable.when, reason:nextAvailable.reason, comment:nextAvailable.comment};
         }
-
-        var now = new timezoneJS.Date();
-        now.setTimezone(timezone);
-        now.setTimestampToNow();
-
-        var util = new availability.AvailabilityIterator({
-        	cal : now,
-        	availability : times
-        });
-        if (!util.hasNext())
+        else
         {
-            return {'status': OPENREST_STATUS_STATUS_AVAILABLE, until:Number.MAX_VALUE}; 
+            return {status:OPENREST_STATUS_STATUS_UNAVAILABLE, until:nextAvailable.when, reason:nextUnavailable.reason, comment:nextUnavailable.comment};
         }
-
-        var status = util.next();
-        if (!status.until) {
-        	status.until = Number.MAX_VALUE;
-        }
-        return status;
     };
 
 	return self;
--- openrest4js/openrest4js-helpers/Item.js
+++ openrest4js/openrest4js-helpers/Item.js
@@ -9,33 +9,71 @@ openrest.ItemHelper = openrest.ItemHelper || (function() {
         return -1;
     };
 
-    self.getStatus = function(item, timezone)
+    function getNextTimeStatusIs(timezone, times, wantedStatus, _time)
     {
+        var time = new timezoneJS.Date();
+        time.setTimezone(timezone);
+        time.setTimestampToNow();
+
+        if (_time)
+        {
+            time.setTimezone(_time.getTimezone());
+            time.setTime(_time.getTime());
+        }
+
+        var when = time.getTime() || Number.MAX_VALUE;
+
+        var times = times || {weekly:[], exceptions:[]};
+        var util = new availability.AvailabilityIterator({
+        	cal : time,
+        	availability : times
+        });
+
+        if (!util.hasNext())
+        {
+            return {when:Number.MAX_VALUE};
+        }
+
+        var status = util.next();
+        var reason = status.reason;
+        var comment = status.comment;
+        while (status.status !== wantedStatus)
+        {
+            if (!status.until) return {when:Number.MAX_VALUE};
+
+            when = status.until;
+            reason = status.reason;
+            comment = status.comment;
+
+        	status = util.next();
+        }
+        return {when:when, reason:reason, comment:comment}
+    }
+
+    self.getStatus = function(item, timezone, time)
+    {
+        if ((timezone) && (timezone.getTimezone))
+        {
+            timezone = timezone.getTimezone();
+            time = timezone;
+        }
+
         if (item.inactive)
         {
             return {'status': OPENREST_STATUS_STATUS_UNAVAILABLE, until:Number.MAX_VALUE}; 
         }
 
-        var now = new timezoneJS.Date();
-        now.setTimezone(timezone);
-        now.setTimestampToNow();
+        var nextAvailable = getNextTimeStatusIs(timezone, item.availability, OPENREST_STATUS_STATUS_AVAILABLE, time);
+        var nextUnavailable = getNextTimeStatusIs(timezone, item.availability, OPENREST_STATUS_STATUS_UNAVAILABLE, time);
 
-        var times = item.availability || {weekly:[], exceptions:[]};
-        var util = new availability.AvailabilityIterator({
-        	cal : now,
-        	availability : times
-        });
-        if (!util.hasNext())
+        if (nextAvailable.when < nextUnavailable.when)
         {
-            console.log("AvailabilityIterator >> item availability hasNext returned false!");
-            return {'status': OPENREST_STATUS_STATUS_AVAILABLE, until:Number.MAX_VALUE}; 
+            return {status:OPENREST_STATUS_STATUS_AVAILABLE, until:nextUnavailable.when, reason:nextAvailable.reason, comment:nextAvailable.comment};
         }
-
-        var status = util.next();
-        if (!status.until) {
-        	status.until = Number.MAX_VALUE;
+        else
+        {
+            return {status:OPENREST_STATUS_STATUS_UNAVAILABLE, until:nextAvailable.when, reason:nextUnavailable.reason, comment:nextUnavailable.comment};
         }
-        return status;
     };
 
     self.getUrl = function(item, local, distributorId)
